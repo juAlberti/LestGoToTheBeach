01b42f404ed6ca9da3d9291f8920f62c5014ba1e
juAlberti <julia.maia@acad.pucrs.br>
2021-11-30 14:34:35 -0300

    first commit

---
+++ Shared/LetsGoToTheBeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsGoToTheBeachApp.swift
+//  Shared
+//
+//  Created by Julia Alberti Maia on 30/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LetsGoToTheBeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
