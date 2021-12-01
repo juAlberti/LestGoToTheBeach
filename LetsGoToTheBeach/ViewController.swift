c96f7a5e31f19f4e5cfeb5691d65b4a491a0dd65
Aline Escobar <21069293+alineescobar@users.noreply.github.com>
2021-12-01 14:56:54 -0300

    modal setting

--- LetsGoToTheBeach/ViewController.swift
+++ LetsGoToTheBeach/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var animationView: AnimationView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var findOutButton: UIButton!
+    @IBOutlet weak var reloadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hellor")
+    
+        animationView!.animationSpeed = 0.8
+        
     }
 
+    private func showBeach() {
+        animationView?.play(fromFrame: 17, toFrame: 45, loopMode: .none)
+    }
+    
+    private func showSnow() {
+        animationView?.play(fromFrame: 51, toFrame: 75, loopMode: .none)
+    }
 
+    @IBAction func findOutButton(_ sender: Any) {
+        performSegue(withIdentifier: "searchBarScreen", sender: nil)
+    }
+    
+    @IBAction func reloadButton(_ sender: Any) {
+    }
 }
 
